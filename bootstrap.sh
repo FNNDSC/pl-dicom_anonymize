c5f0eb593dc198bfa3ad8b59e9ff831b27711792
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2026-07-24 10:24:22 -0400

    Configure python-chrisapp-template/bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,9 +15,9 @@
 # ----------------------------------------
 
 PLUGIN_NAME="$(basename $(dirname $(realpath $0)))"  # name of current directory
-PLUGIN_TITLE='My ChRIS Plugin'
-SCRIPT_NAME='commandname'
-DESCRIPTION='A ChRIS plugin to do something awesome'
+PLUGIN_TITLE='A ChRIS plugin to anonymize DICOM files'
+SCRIPT_NAME='dicom_anonymize'
+DESCRIPTION='A ChRIS plugin to anonymize header metadata in DICOM files'
 ORGANIZATION='FNNDSC'
 EMAIL='dev@babyMRI.org'
 
@@ -46,7 +46,7 @@ ENABLE_ACTIONS_BUILD=yes
 # STEP 2. Uncomment the line where it says READY=yes
 # ----------------------------------------
 
-#READY=yes
+READY=yes
 
 # ----------------------------------------
 # STEP 3. Run: ./bootstrap.sh
